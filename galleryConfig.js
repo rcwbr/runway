476a7e70b66eafa202f37ec993da1a7afe972b30
Eric Weber <eric@weber.lol>
2018-07-08 19:23:36 -0700

    add image scale math and configuration

--- galleryConfig.js
+++ galleryConfig.js
@@ -1,9 +1,16 @@
 const deepmerge = require('deepmerge')
 
+// default gallery config -- values will be overridden by gallery config file
 const defaults = {
 	name: 'gallery',
 	imagesFolder: 'images',
 	thumbsFolder: 'images/thumbs',
+	width: 960,
+	imagesPerRow: 4,
+	imageMargins: {
+		horiz: 10,
+		vert: 10
+	},
 	images: []
 }
 
